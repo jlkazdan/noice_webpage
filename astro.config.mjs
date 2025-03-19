dcfde4c180f7bdc354171d14f599743e691b235a
jlkazdan <30814612+jlkazdan@users.noreply.github.com>
2025-03-19 11:27:07 -0700

    add webpage files from robustness

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
+
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://Avinandan22.github.io',
+  base: '/Robustness/',
+  integrations: [tailwind(), icon(), mdx(), react(), svelte()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    }
+  }
+});
